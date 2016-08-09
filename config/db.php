a5b0a5f9b476d8d4c0600b0c1b80b2965db9f53c
Mark Wong <fmarkwong@gmail.com>
2016-08-08 22:04:07 -0700

    Heroku config and .gitignore

--- config/db.php
+++ config/db.php
@@ -1,11 +1,27 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=economizzer',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-    //'tablePrefix' => 'tb_',
-    'enableSchemaCache' => true,
-];
+if (YII_ENV === 'production') {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url['host'];
+    $db = substr($url["path"], 1);
+
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => "mysql:host=$host;dbname=$db",
+        'username' => $url['user'], 
+        'password' => $url['pass'], 
+        'charset' => 'utf8',
+        //'tablePrefix' => 'tb_',
+        'enableSchemaCache' => true,
+    ];
+} else {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=economizzer',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+        //'tablePrefix' => 'tb_',
+        'enableSchemaCache' => true,
+    ];
+}
--- web/index-test.php
+++ web/index-test.php
@@ -1,6 +1,10 @@
 <?php
 
 // NOTE: Make sure this file is not accessible when deployed to production
+if (getenv('HEROKU_ENV') === 'production') {
+    die('You are not allowed to access this file.');
+}
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
 }
